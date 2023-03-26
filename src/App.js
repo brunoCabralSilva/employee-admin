89232e19f051d09787d6e0e47331d5f136227bb9
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2023-03-26 14:29:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,177 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import firebaseConfig from './back-end/connection';
+import {
+  getFirestore,
+  collection,
+  deleteDoc,
+  getDocs,
+  addDoc,
+  doc,
+  setDoc,
+  where,
+  query,
+} from 'firebase/firestore/lite';
 import './App.css';
 
-function App() {
+const db = getFirestore(firebaseConfig);
+
+export default function App() {
+  const [id, setId] = useState('');
+  const [data, setData] = useState([]);
+  const [name, setName] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [salary, setSalary] = useState(0);
+  const [sector, setSector] = useState('');
+  const [listOfSector, setListOfSector] = useState([]);
+  
+  useEffect(() => {
+    getCollection();
+  }, []);
+  
+  const getCollection = async () => {
+    const collectionEmployee = collection(db, "employee")
+
+    const querySnapshot = await getDocs(collectionEmployee);
+    
+    const data = await querySnapshot._docs.map((item) => {
+      return {
+        name: item._document.data.value.mapValue.fields.name.stringValue,
+        cpf: item._document.data.value.mapValue.fields.cpf.stringValue,
+        salary: item._document.data.value.mapValue.fields.salary.doubleValue,
+        sector: item._document.data.value.mapValue.fields.sector.arrayValue.values.map((element) => element.stringValue),
+      };
+    });
+    setData(data);
+  };
+
+  const addEmployee = async () => {
+    if (id === '') {
+      const collectionEmployee = collection(db, "employee");
+      await addDoc((collectionEmployee), {
+        name,
+        cpf,
+        salary: Number(salary),
+        sector: listOfSector,
+      });
+    } else {
+      await setDoc(doc(db, "employee", id), {
+        name,
+        cpf,
+        salary: Number(salary),
+        sector: listOfSector,
+      });
+    }
+
+    setName('');
+    setCpf('');
+    setSalary(0);
+    setSector('');
+    setListOfSector([]);
+    setId('')
+    getCollection();
+  };
+
+  const editItem = async (element) => {
+    const collectionEmployee = collection(db, "employee");
+    const q = query(collectionEmployee, where("cpf", "==", element.cpf));
+    const querySnapshot = await getDocs(q);
+    setName(querySnapshot.docs[0]._document.data.value.mapValue.fields.name.stringValue);
+    setCpf(querySnapshot.docs[0]._document.data.value.mapValue.fields.cpf.stringValue);
+    setSalary(querySnapshot.docs[0]._document.data.value.mapValue.fields.salary.doubleValue);
+    setListOfSector(querySnapshot.docs[0]._document.data.value.mapValue.fields.sector.arrayValue.values.map((element) => element.stringValue));
+    setId(querySnapshot.docs[0]._key.path.segments[querySnapshot.docs[0]._key.path.segments.length -1]);   
+  };
+
+  const removeItem = async (element) => {
+    const collectionEmployee = collection(db, "employee");
+    const q = query(collectionEmployee, where("cpf", "==", element.cpf));
+    const querySnapshot = await getDocs(q);
+    const id = querySnapshot.docs[0]._key.path.segments[querySnapshot.docs[0]._key.path.segments.length -1];
+    await deleteDoc(doc(db, "employee", id));
+    getCollection();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="title">Cadastrar um usuário</div>
+      <div className="register">
+        <input
+          type="text"
+          onChange={ (e) => setName(e.target.value) }
+          placeholder="Nome"
+          value={name}
+        />
+        <input
+          type="text"
+          onChange={ (e) => setCpf(e.target.value) }
+          placeholder="CPF"
+          value={cpf}
+        />
+        <input
+          type="number"
+          onChange={ (e) => setSalary(e.target.value) }
+          placeholder="Salário"
+          value={salary}
+        />
+        <div>
+          <input
+            type="text"
+            onChange={ (e) => setSector(e.target.value) }
+            placeholder="Setor"
+            value={sector}
+          />
+          <button onClick={() => {
+            setListOfSector([...listOfSector, sector]);
+            setSector('');
+          }}>
+            Adicionar setor
+          </button>
+          <div className="list-of-sectors">
+            {
+              listOfSector.map((element, index) => <span key={ index }>{ element }</span>)
+            }
+          </div>
+        </div>
+        <button onClick={ addEmployee } >
+          {id === '' ? 'Adicionar' : 'Concluir Edição'}
+        </button>
+      </div>
+      <div className="title">Lista de Usuários</div>
+      <div className="list-employees">
+        {
+          data.map((element, index) => (
+            <div className="employee" key={ index }>
+              <div> <p>Nome:</p> { element.name } </div>
+              <div> <p>CPF:</p> { element.cpf } </div>
+              <div> <p>Salário:</p> R${' '}{ element.salary } </div>
+              <div>
+                <p>Lista de Setores:</p>
+                {
+                  element.sector.map((item, index) => {
+                    if (index === element.sector.length - 2) return (<span key={index}>{item}{' e '}</span>);
+                    if (index !== element.sector.length - 1) return (<span key={index}>{item}{', '}</span>);
+                    return (<span key={index}>{item}{'.'}</span>);
+                  })
+                }
+              </div>
+              <button
+                onClick={() => editItem(element)}
+                className="button-update"
+                type="button"
+              >
+                Editar  
+              </button>
+              <button
+                onClick={() => removeItem(element)}
+                className="button-remove"
+                type="button"
+              >
+                Excluir  
+              </button>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/back-end/connection.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = initializeApp ({
+  apiKey: "AIzaSyCkpdUdZHEqqLCkWWmOnVyd9cND1brqlKY",
+  authDomain: "employee-register-9791c.firebaseapp.com",
+  projectId: "employee-register-9791c",
+  storageBucket: "employee-register-9791c.appspot.com",
+  messagingSenderId: "50405060368",
+  appId: "1:50405060368:web:f70abcd94c1ffcbb3bbf95",
+  measurementId: "G-ELSMXVX8SL"
+});
+
+export default firebaseConfig;
